c08e11a23d2ec5b25dde404e8aa4de6ff8a78562
obinna <127059513+obinnameso@users.noreply.github.com>
2024-11-23 22:54:17 +0100

    script and requirements

---
+++ ingestion.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
+
+## load document
+loader = PyPDFLoader("data/terdoo_report.pdf")
+document = loader.load() 
+
+## split into chunks 
+text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=100)
+texts = text_splitter.split_documents(document)
+print(f"created {len(texts)} chunks")
+
